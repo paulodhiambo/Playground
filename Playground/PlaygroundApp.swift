dd1c64e46c5fad0baf136736e153282178dbaf88
Paul Odhiambo <paulodhiambo962@gmail.com>
2022-03-11 11:12:56 +0300

    Initial Commit

---
+++ Playground/PlaygroundApp.swift
@@ -0,0 +1,17 @@
+//
+//  PlaygroundApp.swift
+//  Playground
+//
+//  Created by Paul Odhiambo on 11/03/2022.
+//
+
+import SwiftUI
+
+@main
+struct PlaygroundApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
